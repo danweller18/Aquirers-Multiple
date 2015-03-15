14556a7668b48d57a6e94d483ec280f90636eca0
Daniel Weller <Daniel.Weller@betterbusiness.solutions>
2015-03-15 10:11:01 -0400

    Add static pages with test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def stocks
   end
+
+  def about
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Aquirer's Multiple App"
   end
 
   test "should get stocks" do
     get :stocks
     assert_response :success
+    assert_select "title", "Stocks | Aquirer's Multiple App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Aquirer's Multiple App"
+  end
 end
